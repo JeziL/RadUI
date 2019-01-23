98f7605abfae89a88e72c6b8e885597c98e0f1e3
Li <me@wanjinli.com>
2019-01-23 14:20:45 +0800

    Fix: adv_fit flow control.

--- src/main/python/radui.py
+++ src/main/python/radui.py
@@ -351,10 +351,11 @@ class RadUIForm(QMainWindow):
         self.axes.set_xlabel(AX_LABEL[self.adv_x])
         self.axes.set_ylabel(AX_LABEL[self.adv_y])
         self.axes.scatter(data[self.adv_x], data[self.adv_y], s=5)
-        if self.adv_fit and self.adv_x == "radialDistance" and self.adv_y in ["azimuth", "elevation", "x", "y", "z"]:
-            self.rad.plot_fitting(self.adv_y, self.axes, rad_id)
-        elif self.adv_x in ["x", "y", "z"] and self.adv_y in ["x", "y", "z"]:
-            self.rad.plot_fitting(self.adv_y, self.axes, rad_id, x=self.adv_x)
+        if self.adv_fit:
+            if self.adv_x == "radialDistance" and self.adv_y in ["azimuth", "elevation", "x", "y", "z"]:
+                self.rad.plot_fitting(self.adv_y, self.axes, rad_id)
+            elif self.adv_x in ["x", "y", "z"] and self.adv_y in ["x", "y", "z"]:
+                self.rad.plot_fitting(self.adv_y, self.axes, rad_id, x=self.adv_x)
 
         self.axes.set_title("{0} 号雷达".format(rad_id))
         self.canvas.draw()
